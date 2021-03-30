60f90efabb235bc15364996f2d46bced64c79868
nathanww <nathanww@gmail.com>
2021-03-29 19:22:53 -0500

    increase sleep onset delay and decrease sound offset

--- app/src/main/java/neurelectrics/fitbitdatalogger/MainActivity.java
+++ app/src/main/java/neurelectrics/fitbitdatalogger/MainActivity.java
@@ -76,11 +76,11 @@ public class MainActivity extends AppCompatActivity {
     float E_STOP=0.3f; //emergency stop cueing
     int BACKOFF_TIME=5*60000;
     int MAX_STIM=2000;
-    float CUE_NOISE_OFFSET=0.01f; //how much louder is the cue than the white noise
-    float CUE_NOISE_MAX=0.01f; //how much louder can the cues get than white noise
+    float CUE_NOISE_OFFSET=0.00f; //how much louder is the cue than the white noise
+    float CUE_NOISE_MAX=0.00f; //how much louder can the cues get than white noise
     float MAX_ADAPTION_STEP=0.015f; //If cues seem to trigger a wakeup, drop the max volume we can reach by this much
-    long ONSET_DELAY=15*60*1000; //minimumj delay before cues start
-    long OFFSET_DELAY=3*60*60*1000;
+    long ONSET_DELAY=60*60*1000; //minimumj delay before cues start
+    long OFFSET_DELAY=4*60*60*1000;
     boolean DEBUG_MODE=false; //if true, app simulates stage 3 sleep
     long turnedOnTime=0;
     int above_thresh=0;
